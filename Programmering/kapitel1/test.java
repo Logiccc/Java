8f5935c550899c93cdc63c932c589e0c89bd3410
David Persson <peda16@nti.umea>
2017-09-11 10:35:47 +0200

    test.java

---
+++ Programmering/kapitel1/test.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class test {
+	public static void main(String[]args) {
+		Toolkit.getDefaultToolkit().beep();
+		JOptionPane.showMessageDialog(null, "David Persson" + "ja fucking ha"
+		Toolkit.getDefaultToolkit().beep();
+		JOptionPane.showMessageDialog(null, "112 litel aina");
+		
+
+
+
+	}
+}
