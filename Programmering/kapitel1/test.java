4a98d390ec885948e222c1a19da2cc823323a6fa
David Persson <peda16@nti.umea>
2017-09-11 14:12:55 +0200

    menjaha

--- Programmering/kapitel1/test.java
+++ Programmering/kapitel1/test.java
@@ -4,12 +4,11 @@ import java.util.*;
 
 public class test {
 	public static void main(String[]args) {
-		Toolkit.getDefaultToolkit().beep();
-		JOptionPane.showMessageDialog(null, "Hello guys!" +
-			"\nMe name Logic.");
-		Toolkit.getDefaultToolkit().beep();
-		JOptionPane.showMessageDialog(null, "I am insane counter strick");
-		JOptionPane.showMessageDialog(null, "Datum och tid: * \n" + Calendar.getInstance().getTime().toString());
+		Toolkit.getDefaultToolkit().beep(); //Ljud effekt
+		JOptionPane.showMessageDialog(null, "Hello guys!" + "\nMe name Logic."); //1sta meddelandet
+ 		Toolkit.getDefaultToolkit().beep(); //Ljud effekt nr2
+		JOptionPane.showMessageDialog(null, "I am insane counter strick"); //Andra meddelandet
+		JOptionPane.showMessageDialog(null, "Datum och tid:  \n" + Calendar.getInstance().getTime().toString()); //Kalender
 		//This is kommentar
 		/* Multi line kommentar
 		/123
