edf97bf58839da04e32483298384e2fdbc90b35a
David Persson <peda16@nti.umea>
2017-09-11 11:12:00 +0200

    men jaha

--- Programmering/kapitel1/test.java
+++ Programmering/kapitel1/test.java
@@ -1,15 +1,22 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
 
 public class test {
 	public static void main(String[]args) {
 		Toolkit.getDefaultToolkit().beep();
-		JOptionPane.showMessageDialog(null, "David Persson" + "ja fucking ha"
+		JOptionPane.showMessageDialog(null, "Hello guys!" +
+			"\nMe name Logic.");
 		Toolkit.getDefaultToolkit().beep();
-		JOptionPane.showMessageDialog(null, "112 litel aina");
-		//Test kommentar
-		//Ja ha
-		
+		JOptionPane.showMessageDialog(null, "I am insane counter strick");
+		JOptionPane.showMessageDialog(null, "Datum och tid: * \n" + Calendar.getInstance().getTime().toString());
+		//This is kommentar
+		/* Multi line kommentar
+		/123
+		/123
+		*/
+
+
 
 
 
