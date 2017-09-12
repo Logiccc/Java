355229efba6fc0e4aadc895ab33010af511d802c
David Persson <peda16@nti.umea>
2017-09-12 12:11:06 +0200

    Uppdatering 2017-09-12

--- Programmering/kapitel1/test.java
+++ Programmering/kapitel1/test.java
@@ -4,19 +4,26 @@ import java.util.*;
 
 public class test {
 	public static void main(String[]args) {
-		Toolkit.getDefaultToolkit().beep(); //Ljud effekt
+		//Toolkit.getDefaultToolkit().beep(); //Ljud effekt
 		JOptionPane.showMessageDialog(null, "Hello guys!" + "\nMe name Logic."); //1sta meddelandet
- 		Toolkit.getDefaultToolkit().beep(); //Ljud effekt nr2
+ 		//Toolkit.getDefaultToolkit().beep(); //Ljud effekt nr2
 		JOptionPane.showMessageDialog(null, "I am insane counter strick"); //Andra meddelandet
-		JOptionPane.showMessageDialog(null, "Datum och tid:  \n" + Calendar.getInstance().getTime().toString()); //Kalender
+		JOptionPane.showMessageDialog(null, "Datum och tid: \n" + Calendar.getInstance().getTime().toString()); //Kalender
+		String s1;
+		s1 = "What's up guys";
+		String s2;
+		s2 = " It's ya boi";
+		String s3; 
+		s3 = "  goDazed";
+		String s4; 
+		s4 = "\n";
+		JOptionPane.showMessageDialog(null, s1 + s4 + s2 + s4 + s3);
 		//This is kommentar
-		/* Multi line kommentar
-		/123
-		/123
+		/* Multiline kommentar test1
+		/
+		/
+		/
 		*/
-
-
-
 
 
 	}
